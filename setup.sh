fa9ae92927e3e082dd1fb79eff8dba016bc77082
Answer35 <rbanswer35@gmail.com>
2026-03-08 01:22:38 +0100

    setup.sh: Adding Fedora setup

--- setup.sh
+++ setup.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 # port requirements
 
-if [ "$(id -u)" != "0" ] && [ "$(uname -m)" == "x86_64" ]  && [  "$(uname)" == "Linux" ];then
+if [ "$(id -u)" != "0" ] && [ "$(uname -m)" == "x86_64" ] && [ "$(uname)" == "Linux" ]; then
     echo "请以 root 用户运行"
     echo "please run with sudo"
     exit
 fi
 
-if [ "$(uname -m)" == "x86_64" ] && [  "$(uname)" == "Linux" ];then
-    echo "Device arch: Linux x86_64"
+OS="$(uname)"
+ARCH="$(uname -m)"
+
+if [ "$OS" == "Linux" ]; then
+    . /etc/os-release
+fi
+
+# Debian / Ubuntu
+if [ "$OS" == "Linux" ] && [ "$ARCH" == "x86_64" ] && [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+    echo "Device arch: Linux x86_64 (Debian/Ubuntu)"
     apt update -y
     apt upgrade -y
     apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-21-jre zstd bc android-sdk-libsparse-utils xmlstarlet
-    if [ $? -ne 0 ];then
-        echo "安装可能出错，请手动执行：apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-21-jre zstd bc xmlstarlet"
+
+    if [ $? -ne 0 ]; then
+        echo "安装可能出错，请手动执行："
+        echo "apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-21-jre zstd bc xmlstarlet"
     fi
 fi
 
-if [ "$(uname -m)" == "aarch64" ];then
-    echo "Device arch: aarch64"
-    apt update -y
-    apt upgrade -y
-    apt install -y python busybox zip unzip p7zip openjdk-21 zipalign zstd xmlstarlet
+# Fedora
+if [ "$OS" == "Linux" ] && [ "$ARCH" == "x86_64" ] && [ "$ID" == "fedora" ]; then
+    echo "Device arch: Linux x86_64 (Fedora)"
+    dnf upgrade -y
+    dnf install -y aria2 python3 busybox zip unzip p7zip p7zip-plugins java-21-openjdk zstd bc android-tools xmlstarlet
 fi
 
-if [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "x86_64" ];then
-    echo "Devcie arch: MacOS X86_X64"
-    pip3 install buysbox
+# aarch64 (Debian/Ubuntu)
+if [ "$OS" == "Linux" ] && [ "$ARCH" == "aarch64" ] && [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+    echo "Device arch: Linux aarch64"
+    apt update -y
+    apt upgrade -y
+    apt install -y python3 busybox zip unzip p7zip-full openjdk-21-jre zipalign zstd xmlstarlet
+fi
+
+# MacOS
+if [ "$OS" == "Darwin" ] && [ "$ARCH" == "x86_64" ]; then
+    echo "Device arch: MacOS x86_64"
+    pip3 install busybox
     brew install aria2 openjdk zstd coreutils gdu gnu-sed gnu-getopt grep xmlstarlet
 fi
